36d2d3c4253b2f33587397da855ab97afe9ee768
phcsilva056 <phc.silva@outlook.com.br>
2022-06-10 20:28:26 -0300

    WhatsLab

---
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,169 @@
+import React from "react";
+import styled from "styled-components";
+import BoxFooter from "./components/BoxFooter/BoxFooter";
+import iconLogo from "./img/logo.png";
+import CardMessage from "./components/CardMessage/CardMessage";
+import CardMessageOnlyMe from "./components/CardMessage/CardMessageOnlyMe";
+
+const MainContainer = styled.div`
+  width: 600px;
+  height: 100vh;
+  padding: 0;
+  margin: 0 auto;
+  border: 1px solid gray;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fe7e02;
+  border-bottom: 2px solid gray;
+  position: fixed;
+  top: 0;
+  width: 598px;
+  height: 60px;
+`;
+
+const Logo = styled.img`
+  user-select: none;
+  width: 60px;
+`;
+
+const Logo2 = styled.p`
+  user-select: none;
+  font-size: 26px;
+  margin: 0;
+  font-weight: 600;
+`;
+
+const Footer = styled.footer`
+  background-color: #fe7e02;
+  border-top: 2px solid gray;
+  position: fixed;
+  bottom: 0;
+  width: 598px;
+  height: 80px;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  background-color: #fae2cd;
+  gap: 8px;
+  width: 598px;
+  min-height: 98vh;
+  padding: 60px 16px 80px 16px;
+`;
+
+class App extends React.Component {
+  state = {
+    valorInputUser: "",
+    valorInputMessage: "",
+    messageUser: [
+      {
+        user: "Eu",
+        message: "Olá",
+      },
+      {
+        user: "Zé",
+        message: "Opa",
+      },
+    ],
+  };
+
+  onClickDelete = () =>{
+    if(window.confirm("Deseja deletar?")){
+
+    }
+  }
+
+  onChangeUser = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      valorInputUser: event.target.value,
+    });
+  };
+
+  onChangeMessage = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      valorInputMessage: event.target.value,
+    });
+  };
+
+  onClickSend = () => {
+    const element = document.getElementById("FocarFim");
+    element.scrollIntoView(false);
+    let arraySegura = [this.state.valorInputUser, this.state.valorInputMessage];
+    if (arraySegura[0] !== "") {
+      if (arraySegura[1] != "") {
+        let arrayMessage = [...this.state.messageUser];
+        arrayMessage.push({
+          user: arraySegura[0],
+          message: arraySegura[1],
+        });
+        this.setState({
+          messageUser: [...arrayMessage],
+          valorInputMessage: "",
+        });
+      }
+    }
+  };
+
+  onKeyPressSend = (event) => {
+    if (event.key === "Enter") {
+      const element = document.getElementById("FocarFim");
+      element.scrollIntoView(false);
+      let arraySegura = [
+        this.state.valorInputUser,
+        this.state.valorInputMessage,
+      ];
+      if (arraySegura[0] !== "") {
+        if (arraySegura[1] != "") {
+          let arrayMessage = [...this.state.messageUser];
+          arrayMessage.push({
+            user: arraySegura[0],
+            message: arraySegura[1],
+          });
+          this.setState({
+            messageUser: [...arrayMessage],
+            valorInputMessage: "",
+          });
+        }
+      }
+    }
+  };
+
+  render() {
+    const arrayCardMensagens = this.state.messageUser.map((item, index) => {
+      if (item.user.toLowerCase() === "eu") {
+        return <CardMessageOnlyMe key={index} itens={item} index={index} onClickDelete={this.onClickDelete} />;
+      } else {
+        return <CardMessage key={index} itens={item} index={index} onClickDelete={this.onClickDelete}/>;
+      }
+    });
+    return (
+      <MainContainer>
+        <Header>
+          <Logo src={iconLogo} alt="Logo WhatsLab" />
+          <Logo2>WhatsLab</Logo2>
+        </Header>
+        <Main id="FocarFim">{arrayCardMensagens}</Main>
+        <Footer>
+          <BoxFooter
+            onKeyPressSend={this.onKeyPressSend}
+            onClickSend={this.onClickSend}
+            onChangeMessage={this.onChangeMessage}
+            onChangeUser={this.onChangeUser}
+            valorInputMessage={this.state.valorInputMessage}
+            valorInputUser={this.state.valorInputUser}
+          />
+        </Footer>
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
---
+++ modulo2/projeto-whatslab/whatslab/src/components/CardMessage/CardMessageOnlyMe.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const CardOnlyMe = styled.div`
+  background-color: #fff;
+  min-width: min-content;
+  max-width: max-content;
+  word-wrap: break-word;
+  word-break: break-all;
+  text-align: justify;
+  border-radius: 12px;
+  border: gray 2px solid;
+  background-color: orange;
+  max-width: 70%;  
+  display: grid;
+  h3,p{
+    margin: 0;
+    padding: 8px;
+  }
+  button{
+    background-color: transparent;
+    border-radius:30px ;
+    border: 1px solid gray;
+  }
+`;
+const DivCardOnlyMe = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: flex-end;
+`;
+export default class CardMessageOnlyMe extends Component {
+  render() {
+    return (
+        <DivCardOnlyMe>
+        <CardOnlyMe key={this.props.index}>
+        <h3>{this.props.itens.user}:</h3>
+        <p>{this.props.itens.message}</p>
+        <button onClick={this.props.onClickDelete}>X</button>
+      </CardOnlyMe>
+        </DivCardOnlyMe>
+    )
+  }
+}
