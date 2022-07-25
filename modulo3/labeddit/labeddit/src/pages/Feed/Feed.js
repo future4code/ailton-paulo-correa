22c529c4d00d02f1852ffe3cce6a60417c42636a
phcsilva056 <phc.silva@outlook.com.br>
2022-07-25 20:39:22 -0300

    page feed

--- modulo3/labeddit/labeddit/src/pages/Feed/Feed.js
+++ modulo3/labeddit/labeddit/src/pages/Feed/Feed.js
@@ -1,19 +1,55 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import { BoxPost, ButtonNewPost, ContarinerFeed, LineDivisorPost, SentBy, TextBoxPost } from './style'
+import React from "react";
+import Header from "../../components/Header/Header";
+import IconComment from "../../assets/comments.png";
+import IconLike from "../../assets/like.png";
+import IconUnlike from "../../assets/unlike.png";
+import {
+  BoxComments,
+  BoxInteraction,
+  BoxPost,
+  ButtonNewPost,
+  ContarinerFeed,
+  CountPost,
+  DivBoxs,
+  Divisor50,
+  IconInteraction,
+  LineDivisorPost,
+  Posts,
+  PostText,
+  SentBy,
+  TextBoxPost,
+} from "./style";
 
 export default function Feed() {
   return (
     <>
-    <Header/>
-    <ContarinerFeed>
-      <TextBoxPost placeholder='Escreva seu post...'/>      
-      <ButtonNewPost>Postar</ButtonNewPost>
-      <LineDivisorPost />
-      <BoxPost>
-        <SentBy>Enviado por: User</SentBy>
-      </BoxPost>
-    </ContarinerFeed>
+      <Header />
+      <ContarinerFeed>
+        <TextBoxPost placeholder="Escreva seu post..." />
+        <ButtonNewPost>Postar</ButtonNewPost>
+        <LineDivisorPost />
+        <Posts>
+          <Divisor50/>
+          <BoxPost>
+            <SentBy>Enviado por: User</SentBy>
+            <PostText>
+              Porque a maioria dos desenvolvedores usam Linux? ou as empresas de
+              tecnologia usam Linux?
+            </PostText>
+            <DivBoxs>
+              <BoxInteraction>
+                <IconInteraction src={IconLike} />
+                <CountPost>0</CountPost>
+                <IconInteraction src={IconUnlike} />
+              </BoxInteraction>
+              <BoxComments>
+                <IconInteraction src={IconComment} />
+                <CountPost>0</CountPost>
+              </BoxComments>
+            </DivBoxs>
+          </BoxPost>
+        </Posts>
+      </ContarinerFeed>
     </>
-  )
+  );
 }
--- modulo3/labeddit/labeddit/src/pages/Feed/style.js
+++ modulo3/labeddit/labeddit/src/pages/Feed/style.js
@@ -5,6 +5,7 @@ export const ContarinerFeed = styled.main`
   display: flex;
   flex-direction: column;
   padding: 32px 32px 0px 32px;
+  max-height: 100vh;
 `;
 
 export const TextBoxPost = styled.textarea`
@@ -32,7 +33,18 @@ export const ButtonNewPost = styled(Button1)`
 `;
 
 export const LineDivisorPost = styled(LineDivisor)`
-  margin: 12px 0 50px 0;
+  margin-top: 12px;
+  margin-bottom: 0px;
+`;
+
+export const Posts = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+`;
+
+export const Divisor50 = styled.div`
+  min-height: 50px;
 `;
 
 export const BoxPost = styled.div`
@@ -56,4 +68,47 @@ export const SentBy = styled.p`
   font-size: 12px;
   line-height: 16px;
   text-align: center;
+`;
+
+export const PostText = styled.p`
+  font-family: "IBM Plex Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+`;
+
+export const DivBoxs = styled.div`
+  display: flex;
+  gap: 11px;
+`;
+
+export const BoxInteraction = styled.div`
+  border: 0.8px solid #ececec;
+  border-radius: 28px;
+  display: flex;
+  gap: 14px;
+  padding: 7px 12px;
+`;
+export const BoxComments = styled.div`
+  border: 0.8px solid #ececec;
+  border-radius: 28px;
+  display: flex;
+  gap: 10px;
+  padding: 7px 12px;
+`;
+
+export const IconInteraction = styled.img`
+  width: 15px;
+  height: 14px;
+`;
+
+export const CountPost = styled.p`
+  font-family: "IBM Plex Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 12px;
+  text-align: center;
+  color: #6f6f6f;
 `;
